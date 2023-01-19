f4e30326c45a51deebf8dc62c617b07489e4b5db
Akash Maurya <Akashm@agu.edu.bh>
2023-01-19 13:29:53 +0300

    if Else in vue is completed

--- src/components/IfElse.vue
+++ src/components/IfElse.vue
@@ -1,24 +1,40 @@
-<template >
+<template>
     <div>
-        <h1 v-if="show"> If Condition</h1>
+        <!-- <h1 v-if="show"> If Condition</h1>
         <h1 v-else> Else Condition</h1>
-      
+
         <br>
         <br>
-        <button v-on:click="show=!show">Toggle</button>
+        <button v-on:click="show = !show">Toggle</button>
+
+        <hr> -->
+        <br>
+
+        <input type="text" maxlength="2" v-model="checkPersonAge"
+            oninput="this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*?)\..*/g, '$1');">
+            <h1 v-if="checkPersonAge>=18">Your Are Eligible to drive</h1>
+            <h1 v-else>Your Are not Eligible to drive</h1>
+        <p>{{ checkPersonAge }}</p>
+        <br>
+        <br>
+        <br>
+
     </div>
 </template>
+
 <script>
 export default {
     name: "IfElse",
     data() {
         return {
-            show: true
+            show: true,
+            member: '',
+            checkPersonAge: '18'
         }
     }
-
 }
 </script>
+
 <style scoped>
 
 </style>
